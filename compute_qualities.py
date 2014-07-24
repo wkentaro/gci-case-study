12acb33d5b00dfc0f7e10dfad6d4023234a109d1
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-07-25 03:31:38 +0900

    1 or 0 clustering. output files

--- compute_qualities.py
+++ compute_qualities.py
@@ -9,6 +9,7 @@ import collections
 import numpy as np
 from sklearn import cluster
 from sklearn import preprocessing
+import pandas as pd
 def connect_db():
     """Connect to the database"""
     db = MySQLdb.connect(db=config.db, host=config.host,\
@@ -80,13 +81,14 @@ def compute_qualities():
         for purpose in product_info['purposes']:
             sales_of_purposes[purpose] += product_info['price'] * product_info['review_count']
 
-    # 効用毎の売上割合を計算(高級度)
-    max_sales = max(sales_of_purposes.values())
-    for purpose in sales_of_purposes.keys():
-        sales_of_purposes[purpose] /= max_sales
+    # # 効用毎の売上割合を計算(高級度)
+    # max_sales = max(sales_of_purposes.values())
+    # for purpose in sales_of_purposes.keys():
+    #     sales_of_purposes[purpose] /= max_sales
 
-    # 商品ごとに効用の高級度を計算(販売数をかける)
+    # 商品ごとに効用の高級度を計算
     # このデータを元にクラスタリングを行う
+    counter = collections.defaultdict(int)
     quality_of_products = {}
     for product_id, product_info in products_info.items():
         quality_of_products[product_id] = {}
@@ -95,8 +97,16 @@ def compute_qualities():
                 #ratioof_review_count = product_info['review_count'] / float(all_review_count)
                 #quality_of_products[product_id][purpose] = sales * ratioof_review_count
                 quality_of_products[product_id][purpose] = sales
+                counter[purpose] += 1
             else:
                 quality_of_products[product_id][purpose] = 0.0
+
+    for purpose, sales in sales_of_purposes.items():
+        sales_of_purposes[purpose] /= float(counter[purpose])
+    df = {'purpose': [p.encode('sjis') for p in sales_of_purposes.keys()],
+          'sales': sales_of_purposes.values()}
+    df = pd.DataFrame(df)
+    df.to_csv('purpose_sales.csv')
 
     # クラスタリングのためにXを作成する
     ref = []
@@ -104,13 +114,21 @@ def compute_qualities():
     X_header = quality_of_products.values()[0].keys()
     for product_id, quality in quality_of_products.items():
         ref.append(product_id)
-        X.append(quality.values())
+        Xi = []
+        for q in quality.values():
+            if q == 0:
+                Xi.append(q)
+            else:
+                Xi.append(1.)
+        #X.append(quality.values())
+        X.append(Xi)
     X = np.array(X)
+    print X
 
     # クラスタリングを行う
-    X_scale = preprocessing.scale(X)
+    #X = preprocessing.scale(X)
     km = cluster.MiniBatchKMeans(n_clusters=8, n_init=10)
-    km.fit(X_scale)
+    km.fit(X)
 
     return products_info, ref, km.labels_, X, X_header
 
